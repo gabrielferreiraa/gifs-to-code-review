615d34b2b193c0c78ea3d1ca98d8da9a341e36c8
Gabriel Ferreira <gabriel@lightup.com.br>
2019-06-04 10:13:32 -0300

    add new gifs

--- optimize.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-for filename in os.listdir('.'):
-    if filename.endswith('.gif'):
-       command="convert " + filename + " -fuzz 13% -layers Optimize " + filename
-       os.system(command)
